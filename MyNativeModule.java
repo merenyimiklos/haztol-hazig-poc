6bf87583a997780f5d94931f1544df1c0dbc7962
macbookpro <miklos.merenyi@offsite-solutions.com>
2024-07-08 22:30:06 +0200

    added three java classes to the poc

---
+++ MyNativeModule.java
@@ -0,0 +1,28 @@
+package com.example.react;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class MyNativeModule extends ReactContextBaseJavaModule {
+
+    MyNativeModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "MyNativeModule";
+    }
+
+    @ReactMethod
+    public void callFragmentMethod(String value) {
+        MainActivity activity = (MainActivity) getCurrentActivity();
+        if (activity != null) {
+            activity.updateFromReactNative(value);
+        }
+    }
+}
